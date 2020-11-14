4a5c7e42547612492890e89b8cfeefbecc46b05b
jdeleon <jdeleonferreira@gmail.com>
2020-11-14 17:03:24 -0500

    Activity list finished

--- app/src/main/java/com/example/personas/CrearPersonas.java
+++ app/src/main/java/com/example/personas/CrearPersonas.java
@@ -3,12 +3,41 @@ package com.example.personas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CrearPersonas extends AppCompatActivity {
-
+    private EditText id, name, lastName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_personas);
+
+        id = findViewById(R.id.txtIdentification);
+        name = findViewById(R.id.txtName);
+        lastName = findViewById(R.id.txtLastName);
+    }
+
+    public void save(View v){
+        String id, name, lastName;
+        Persona p;
+
+        id = this.id.getText().toString();
+        name = this.name.getText().toString();
+        lastName = this.lastName.getText().toString();
+
+        p = new Persona(id, name, lastName);
+        p.save();
+
+        Toast.makeText(this, "Persona guardada exitosamente",Toast.LENGTH_LONG).show();
+        this.clear(v);
+    }
+
+    public void clear(View v){
+        this.id.setText("");
+        this.name.setText("");
+        this.lastName.setText("");
+        this.id.requestFocus();
     }
 }
--- app/src/main/java/com/example/personas/ListarPersonas.java
+++ app/src/main/java/com/example/personas/ListarPersonas.java
@@ -2,13 +2,51 @@ package com.example.personas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Person;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListarPersonas extends AppCompatActivity {
+
+    private TableLayout table;
+    private ArrayList<Persona> personas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_personas);
+
+
+        table = (TableLayout) findViewById(R.id.tblPersonas);
+        personas = Datos.obtener();
+
+        for (int i = 0; i < personas.size(); i++) {
+            TableRow row = new TableRow(this);
+
+            TextView num = new TextView(this);
+            TextView id = new TextView(this);
+            TextView name = new TextView(this);
+            TextView lastName = new TextView(this);
+
+            num.setText("" + (i +1));
+            num.setGravity(Gravity.CENTER_HORIZONTAL);
+            id.setText(personas.get(i).getIdentification().toString());
+            name.setText(personas.get(i).getName().toString());
+            lastName.setText(personas.get(i).getLastName().toString());
+
+            row.addView(num);
+            row.addView(id);
+            row.addView(name);
+            row.addView(lastName);
+
+            table.addView(row);
+        }
     }
 }
